b99f43b9f658255238b60a6c554b80f103e31deb
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-25 20:52:55 +0000

    temporary test code for fetchCoordsByIP function

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ const { fetchMyIP } = require('./iss');
 
 //   console.log('It worked! Returned IP:' , ip);
 // });
+
+const { fetchCoordsByIP } = require('./iss');
+
+fetchCoordsByIP('207.195.120.191', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
