b289fa2cc9e616f5b2c885717632645bc5091f0f
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-25 21:45:13 +0000

    temporary test code for fetchISSOverTimes function

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const { fetchMyIP } = require('./iss');
+// const { fetchMyIP } = require('./iss');
+// const { fetchCoordsByIP } = require('./iss');
+// const { fetchISSFlyOverTimes } = require('./iss');
+
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -9,7 +12,6 @@ const { fetchMyIP } = require('./iss');
 //   console.log('It worked! Returned IP:' , ip);
 // });
 
-// const { fetchCoordsByIP } = require('./iss');
 
 // fetchCoordsByIP('207.195.120.191', (error, coordinates) => {
 //   if (error) {
@@ -19,3 +21,15 @@ const { fetchMyIP } = require('./iss');
 
 //   console.log('It worked! Returned coordinates:' , coordinates);
 // });
+
+
+
+// fetchISSFlyOverTimes({ latitude: 50.429, longitude: -104.5341 }, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned flyover times:', passTimes);
+// });
+
