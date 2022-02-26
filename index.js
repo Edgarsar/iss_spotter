b7067d9580df07f2a2ba673a11830a0758ae0103
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-26 00:29:11 +0000

    added printPassTimes function and nextISSTimesForMyLocation function call

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// const { fetchMyIP } = require('./iss');
-// const { fetchCoordsByIP } = require('./iss');
-// const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
 
 // fetchMyIP((error, ip) => {
@@ -32,4 +30,22 @@
 
 //   console.log('It worked! Returned flyover times:', passTimes);
 // });
+
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
 
